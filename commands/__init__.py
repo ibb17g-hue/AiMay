5be488ea18d4179bf11a1b39b3fa3f4f3b4d1fec
ibb17g-hue <ibb17g@gmail.com>
2026-07-13 11:10:45 +0300

    Add new imports for task management features

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,6 +1,6 @@
 """Плагінна архітектура команд AiMay: кожен модуль реєструє себе через @command."""
 
 from .registry import COMMAND_REGISTRY, command
-from . import decl, emit, await_, check, tune, bind, hold, break_, fork, seal, bridge_cmd
+from . import decl, emit, await_, check, tune, bind, hold, break_, fork, seal, bridge_cmd, task, adopt, query
 
 __all__ = ["COMMAND_REGISTRY", "command"]
